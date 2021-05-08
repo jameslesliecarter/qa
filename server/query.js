11f190ad37731ff69bb831dd7870309b9d31c540
Jim Carter <jameslesliecarter@gmail.com>
2021-05-07 20:52:07 -0400

    refactoring answers route

--- server/query.js
+++ server/query.js
@@ -8,18 +8,11 @@ const getPhotos = async(id) => {
 
 const getAnswers = async(id, page, count) => {
   var answers = await db.query(`SELECT * FROM answers WHERE id_questions = ${id}`);
-  answers = answers.splice((page - 1) * 5 , page * 5 + count);
   for (var i = 0; i < answers.length; i ++) {
     answers[i].photos = await getPhotos(answers[i].answer_id);
   }
-  answers = format.photos(answers);
-  answerResults = {
-    'question': id,
-    'page': page,
-    'count': count,
-    results: answers
-  };
-  return answerResults;
+  answers = format.answers(answers, id, page, count);
+  return answers;
 }
 
 
